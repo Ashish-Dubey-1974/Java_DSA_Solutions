93265842b6e210295a3f312b6791f41cd4199cf0
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-11-09 13:42:09 +0530

    Adding new file BST

--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Implementation of BinaryTree
         Implementation tree = new Implementation();
         tree.populate(scanner);
         tree.display();//Fucntion to display a tree
         tree.prettyDisplay();// function to display a tree inn Style (' ^ ')
+
+//---------------------------------------------------------------------------------------------------------------------------\\
+
+        //Implamentation of Binary Search Tree
+        BST BinaryTree = new BST();
+        int[] nums = {6,5,1,8,3,5,4,7};
+		BinaryTree.populate(nums);
+		BinaryTree.display();
+
     }
 }
