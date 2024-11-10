4fe1ca85eeb62d836a9d0fd475d83a98571c6a3b
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-11-10 15:31:11 +0530

    update file

--- BinaryTree/Recursive_PreOrder.java
+++ BinaryTree/Recursive_PreOrder.java
@@ -3,7 +3,7 @@ package BinaryTree;
 import java.util.ArrayList;
 import java.util.List;
 
-class RecursivePreOrder {
+class Recursive_PreOrder {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 4, -1, -1, 5, 6, -1, -1, 7, -1, -1, 3, -1, 8, 9, -1, -1, -1};
